c7d84fd98cfb2d0264830303a5337e0078eb8003
Luiz Monferrari <lmonferrari@outlook.com>
2023-03-05 18:00:51 -0300

    alteração para o postgres

--- projetoFinal/gestao/settings.py
+++ projetoFinal/gestao/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'gestao.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestao',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
