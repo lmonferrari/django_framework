fd7cb21a4daae46eaea370c5f30b8b5ddae39de6
Luiz Monferrari <lmonferrari@outlook.com>
2023-05-05 17:24:09 -0300

    relational

--- projetoFinal/clientes/models.py
+++ projetoFinal/clientes/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 
-# Create your models here.
+class Document(models.Model):
+    num_doc = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.num_doc
+
+
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -11,5 +17,31 @@ class Person(models.Model):
     photo = models.ImageField(
         upload_to='clients_photos', null=True, blank=True)
 
-    def __str__(self):
+    doc = models.OneToOneField(
+        Document, null=True, blank=True, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
         return self.first_name
+
+
+class Product(models.Model):
+    number = models.CharField(max_length=7)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    product = models.CharField(max_length=15)
+
+    def __str__(self) -> str:
+        return self.product
+
+
+class Sale(models.Model):
+    number = models.CharField(max_length=7)
+    value = models.DecimalField(max_digits=5, decimal_places=2)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+
+    person = models.ForeignKey(
+        Person, null=True, blank=True, on_delete=models.PROTECT)
+
+    products = models.ManyToManyField(Product, blank=True)
+
+    def __str__(self) -> str:
+        return self.number
